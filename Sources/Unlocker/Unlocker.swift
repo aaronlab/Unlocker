0913f9b1b0e3ef0f5437ea79738e76e4df691a44
Aaron Lee <aaronlab.net@gmail.com>
2021-02-07 00:21:19 +0900

    feat: init properties

--- Sources/Unlocker/Unlocker.swift
+++ Sources/Unlocker/Unlocker.swift
@@ -2,10 +2,33 @@ import SwiftUI
 
 public struct Unlocker: View {
     
-    public init() {}
+    // Percentage
+    @Binding var percantage: Float
+    private let minPercentage: Float
+    private let threshold: Float
+    
+    // Action
+    private let completion: (() -> Void)?
+    
+    // State
+    @State private var shouldChange: Bool = true
+    
+    public init(
+        percentage: Binding<Float>,
+        minPercentage: Float = 25.0,
+        threshold: Float = 50.0,
+        completion: (() -> Void)? = nil
+    ) {
+        self._percantage = percentage
+        self.minPercentage = minPercentage
+        self.threshold = threshold
+        self.completion = completion
+    }
     
     public var body: some View {
-        Text("Unlocker")
+        GeometryReader { geo in
+            
+        } //: G
     }
     
 }
