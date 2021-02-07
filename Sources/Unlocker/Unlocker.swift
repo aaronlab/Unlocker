8ef3df9264b36f8d6f7813852cf9965be057d340
Aaron Lee <aaronlab.net@gmail.com>
2021-02-07 19:09:54 +0900

    feat: init boilerplate

--- Sources/Unlocker/Unlocker.swift
+++ Sources/Unlocker/Unlocker.swift
@@ -3,7 +3,7 @@ import SwiftUI
 public struct Unlocker: View {
     
     // Percentage
-    @Binding var percantage: Float
+    @Binding var percentage: Float
     private let minPercentage: Float
     private let threshold: Float
     
@@ -19,7 +19,7 @@ public struct Unlocker: View {
         threshold: Float = 50.0,
         completion: (() -> Void)? = nil
     ) {
-        self._percantage = percentage
+        self._percentage = percentage
         self.minPercentage = minPercentage
         self.threshold = threshold
         self.completion = completion
@@ -28,7 +28,59 @@ public struct Unlocker: View {
     public var body: some View {
         GeometryReader { geo in
             
+            // Slider
+            ZStack(alignment: .leading) {
+                
+                Rectangle()
+                    .frame(width: abs(geo.size.width * CGFloat(minPercentage / 100)))
+            } //: Z
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ value in
+                        onChanged(with: value, geoProxy: geo)
+                    })
+                    .onEnded({ value in
+                        
+                    })
+            )
         } //: G
+    }
+    
+}
+
+extension Unlocker {
+    
+    // 슬라이더 변경 감지 처리
+    private func onChanged(with value: DragGesture.Value, geoProxy: GeometryProxy) {
+        
+        if value.translation.width > 0 && shouldChange {
+            
+            if percentage >= 25.0 {
+                
+                DispatchQueue.main.async {
+                    withAnimation(.easeOut) {
+                        percentage = min(max(minPercentage, Float(value.location.x / geoProxy.size.width * 100)), 100)
+                    }
+                }
+            } else {
+                
+                DispatchQueue.main.async {
+                    withAnimation(.easeOut) {
+                        percentage = 25.0
+                    }
+                }
+            }
+        }
+        
+        if value.translation.width < 0 || !shouldChange {
+            
+            DispatchQueue.main.async {
+                withAnimation(.easeOut) {
+                    percentage = 25.0
+                }
+            }
+            
+        }
     }
     
 }
@@ -38,6 +90,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             Unlocker(percentage: .constant(0.0))
+                .frame(height: 60)
+                .padding()
         }
     }
 }
