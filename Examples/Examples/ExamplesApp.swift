6701f6f16d543d4717d91e5f6ec1172fb91fbc21
Aaron Lee <aaronlab.net@gmail.com>
2021-02-07 22:04:31 +0900

    feat: init example

---
+++ Examples/Examples/ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesApp.swift
+//  Examples
+//
+//  Created by Aaron Lee on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
