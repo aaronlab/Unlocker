84124f2347a5fe59c6a6c8fc8594153daf4aa7cd
Aaron Lee <aaronlab.net@gmail.com>
2021-02-20 13:37:33 +0900

    chore: update examples

--- Examples/Examples/CapsuleShape.swift
+++ Examples/Examples/CapsuleShape.swift
@@ -15,32 +15,88 @@ struct CapsuleShape: View {
     
     var body: some View {
         
-        Unlocker(disabled: $disabled, percentage: $percentage, minPercentage: 0.0, threshold: 50.0) {
-            // Your task here
-            print("Process Started")
+        ZStack {
             
-            /*
-             Since the slider will be "disabled" after fully swiped,
-             you will need to toggle "disabled" parameter at the end of your process,
-             so that you will be able to make the slider activated again.
-             Or if you don't want to make it back,
-             just leave it there.
-             Or if you don't want to make it disabled ever,
-             just use `.constant(false)`
-             */
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                print("Process Done")
-                disabled.toggle()
+            Color.gray
+            
+            // Background
+            HStack {
+                Spacer(minLength: 0)
+                Text("Slide to Unlock")
+                Image(systemName: "chevron.right.2")
+            } //: H
+            .padding(.trailing)
+            
+            // Slider
+            Unlocker(disabled: $disabled, percentage: $percentage, minPercentage: 25, threshold: 50.0) { sliderWidth in
+                ZStack {
+                    
+                    // Slider Background
+                    Rectangle()
+                        .foregroundColor(.primary)
+                    
+                    /*
+                     You can choose when you are going to show your content
+                     with the escaping parameter(CGFloat), which is the slider's width.
+                     */
+                    if sliderWidth > UIScreen.main.bounds.width / 3.0 {
+                        
+                        // Slider Content
+                        HStack {
+                            
+                            Text("Slide to Unlock")
+                                .foregroundColor(.blue)
+                                .lineLimit(1)
+                                .padding(.leading)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .foregroundColor(.blue)
+                            
+                            Spacer(minLength: 0)
+                            
+                        } //: H
+                        
+                    } else {
+                        
+                        /*
+                         Placeholder Image before slide
+                         */
+                        Image(systemName: "chevron.right.2")
+                            .foregroundColor(.blue)
+                        
+                    }
+                    
+                }
+            } completion: {
+                // Your task here
+                print("CapsuleShape Process Started")
+                
+                /*
+                 Since the slider will be "disabled" after fully swiped,
+                 you will need to toggle "disabled" parameter at the end of your process,
+                 so that you will be able to make the slider activated again.
+                 Or if you don't want to make it back,
+                 just leave it there.
+                 Or if you don't want to make it disabled ever,
+                 just use `.constant(false)`
+                 */
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    print("CapsuleShape Process Done")
+                    disabled.toggle()
+                }
             }
             
-        }
-        .background(Color.gray)
+            
+        } //: Z
+        .font(.system(size: 12))
+        .frame(height: 60)
         /*
          You can smiply make the slider RoundedRectangle
          with `.clipShape(Capsule())`
          */
         .clipShape(Capsule())
-        .frame(height: 60)
         .padding()
+        
     }
+    
 }
